a6f800559a8bf4ed9521642c0363773226203902
Vishwac Sena Kannan <vkannan@microsoft.com>
2018-04-13 18:52:30 -0700

    More samples stuff

---
+++ Samples/CafeBotDotNet _ 3 _ QnA/CafeBotDotNet/Startup.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Bot.Builder.Ai.LUIS;
+using Microsoft.Bot.Builder.Ai.QnA;
+using Microsoft.Bot.Builder.BotFramework;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Cognitive.LUIS;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Microsoft.Bot.Samples.CafeBotDotNet
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton(_ => Configuration);
+            services.AddBot<HelloBot>(options =>
+            {
+                options.CredentialProvider = new ConfigurationCredentialProvider(Configuration);
+                // If you want to get all intents scorings, add verbose in luisOptions
+                var luisOptions = new LuisRequest { Verbose = true };
+
+                options.Middleware.Add(new LuisRecognizerMiddleware(
+                    new LuisModel(
+                        "798f44a8-fa5a-46bc-8376-05e7f70dd996", 
+                        "be30825b782843dcbbe520ac5338f567", 
+                        new Uri("https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/")), luisOptions: luisOptions));
+
+                options.Middleware.Add(new QnAMakerMiddleware(
+                                new QnAMakerMiddlewareOptions()
+                                {
+                                    SubscriptionKey = "d534abd71a0d438d95d5a001025ee074",
+                                    KnowledgeBaseId = "16a219d7-5d89-4fcb-b013-ab37a6304a32",
+                                    EndActivityRoutingOnAnswer = true
+                                }));
+
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.UseBotFramework();
+        }
+    }
+}
---
+++ Samples/CafeBotDotNet _ 5 _ Prompts/CafeBotDotNet/CafeBotRoot.cs
@@ -0,0 +1,114 @@
+﻿using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder.Ai.LUIS;
+using System;
+using System.Threading;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+using Microsoft.Bot.Builder.Prompts;
+
+namespace Microsoft.Bot.Samples.CafeBotDotNet
+{
+    public class HelloBot : IBot
+    {
+        public async Task OnTurn(ITurnContext context)
+        {
+            switch (context.Activity.Type)
+            {
+                case ActivityTypes.Message:
+
+                    var luisResult = context.Services.Get<RecognizerResult>(LuisRecognizerMiddleware.LuisRecognizerResultKey);
+
+                    if (luisResult != null && !textData.Prompted)
+                    {
+                        (string key, double score) topItem = luisResult.GetTopScoringIntent();
+                        switch(topItem.key)
+                        {
+                            case "Greeting":
+                                await context.SendActivity($"Hello, I'm the contoso cafe bot. How can I help you?");
+                                break;
+                            case "Communication preference":
+                                // get entities
+                                if (luisResult.Entities.Count > 0)
+                                {
+                                    JToken commPreference;
+                                    if(luisResult.Entities.TryGetValue("communicationOption", out commPreference))
+                                    {
+                                        switch(commPreference[0][0].ToString())
+                                        {
+                                            case "call":
+                                                await context.SendActivity("Ok. I'll remember that you prefer to receive phone calls");
+                                                break;
+                                            case "text":
+                                                await context.SendActivity("Ok. I'll remember that you prefer text updates");
+                                                break;
+                                            default:
+                                                await context.SendActivity("Sorry. I do not understand that.");
+                                                await context.SendActivity("You try 'I prefer text messages' or 'I prefer phone calls'");
+                                                break;
+                                        }
+                                    }
+                                }
+                                break;
+                            case "Who are you intent":
+                                GetTextUsingPrompt(context);
+                                break;
+                            default:
+                                await context.SendActivity($"Hello world.");
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        GetTextUsingPrompt(context);
+                    }
+                    break;
+                case ActivityTypes.ConversationUpdate:
+                    foreach (var newMember in context.Activity.MembersAdded)
+                    {
+                        if (newMember.Id != context.Activity.Recipient.Id)
+                        {
+                            await context.SendActivity("Hello and welcome to the Cafe bot.");
+                        }
+                    }
+                    break;
+            }
+        }
+
+        private async void GetTextUsingPrompt(ITurnContext context)
+        {
+            TextPrompt getUserNamePrompt = new TextPrompt();
+            if(!textData.Prompted)
+            {
+                // We haven't prompted yet, so ask for the information and set the flag.
+                await getUserNamePrompt.Prompt(context, "Hi, I'm the Contoso Cafe bot. What's your name?");
+                textData.Prompted = true;
+            }
+            else
+            {
+                TextResult result = await getUserNamePrompt.Recognize(context);
+                if(result.Succeeded())
+                {
+                    textData.Value = result.Value;
+                    await context.SendActivity($"Hello {textData.Value}. Nice to meet you!");
+                }
+                else
+                {
+                    // Validation failed, so reprompt.
+                    await getUserNamePrompt.Prompt(context, "I did not understand that. Please enter your name.");
+                }
+            }
+        }
+        static PromptTextState textData = new PromptTextState();
+        // Internal helper class.
+        private class PromptTextState
+        {
+            public bool Prompted { get; set; } = false;
+            public string Value { get; set; } = string.Empty;
+        }
+    }
+
+    
+
+}
